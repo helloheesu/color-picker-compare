02fa5328c470e80c38999996ed6c45ac7300b8dd
helloheesu <hi.heesu820@gmail.com>
2019-08-04 06:10:32 +0900

    :fire: [color-thief] pv - useless params, methods

--- lib/color-thief/quantize.ts
+++ lib/color-thief/quantize.ts
@@ -12,39 +12,20 @@
  * Copyright 2010 Stanford Visualization Group
  * Licensed under the BSD License: http://www.opensource.org/licenses/bsd-license.php
  */
-if (!pv) {
-  var pv = {
-    map: function(array, f) {
-      var o = {};
-      return f
-        ? array.map(function(d, i) {
-            o.index = i;
-            return f.call(o, d);
-          })
-        : array.slice();
-    },
-    naturalOrder: function(a, b) {
-      return a < b ? -1 : a > b ? 1 : 0;
-    },
-    sum: function(array, f) {
-      var o = {};
-      return array.reduce(
-        f
-          ? function(p, d, i) {
-              o.index = i;
-              return p + f.call(o, d);
-            }
-          : function(p, d) {
-              return p + d;
-            },
-        0
-      );
-    },
-    max: function(array, f) {
-      return Math.max.apply(null, f ? pv.map(array, f) : array);
-    }
-  };
-}
+
+const pv = {
+  naturalOrder: function(a: number, b: number) {
+    return a < b ? -1 : a > b ? 1 : 0;
+  },
+  sum: function(array: number[]) {
+    return array.reduce(function(p, d) {
+      return p + d;
+    }, 0);
+  },
+  max: function(array: number[]) {
+    return Math.max.apply(null, array);
+  }
+};
 
 /**
  * Basic Javascript port of the MMCQ (modified median cut quantization)
