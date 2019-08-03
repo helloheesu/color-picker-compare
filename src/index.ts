5cdc8d8e5f2fe015c10aab27d71f230c8400bd08
helloheesu <hi.heesu820@gmail.com>
2019-08-03 16:49:25 +0900

    :sparkles: apply color-thief

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { init, getDataUrl, getImage } from "./dropzone";
+import ColorThief from "../lib/color-thief";
 
 const imageListContainer = document.querySelector(".image-list");
 
@@ -6,6 +7,21 @@ init(".drop-zone", (file: File) => {
   getDataUrl(file)
     .then((src: string) => getImage(src))
     .then((img: HTMLImageElement) => {
-      imageListContainer.appendChild(img);
+      const container = document.createElement("div");
+      const list = document.createElement("ul");
+
+      container.appendChild(img);
+      container.appendChild(list);
+
+      imageListContainer.appendChild(container);
+
+      const item = document.createElement("li");
+      const color: string = ColorThief.getColor(img).join(", ");
+      const backgroundColor = `rgb(${color})`;
+
+      item.style.backgroundColor = backgroundColor;
+
+      list.appendChild(item);
+      item.innerText = `color-thief: ${color}`;
     });
 });
